ae6d5a3634a8b5f76b6bcb20d6080a77bbe42901
MC-AGANG <54347161+MC-AGANG@users.noreply.github.com>
2021-12-11 13:32:28 +0800

    Add files via upload

---
+++ 图片放大/Form1.Designer.vb
@@ -0,0 +1,227 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.OpenFileDialog2 = New System.Windows.Forms.OpenFileDialog()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Button8 = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(639, 413)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(122, 39)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "启动"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(652, 36)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(84, 28)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "选择图片"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(74, 38)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(552, 21)
+        Me.TextBox1.TabIndex = 2
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 13)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(533, 412)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox1.TabIndex = 3
+        Me.PictureBox1.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(16, 44)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(29, 12)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "路径"
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(642, 479)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(118, 37)
+        Me.Button3.TabIndex = 5
+        Me.Button3.Text = "批处理"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(9, 11)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(76, 25)
+        Me.Button4.TabIndex = 6
+        Me.Button4.Text = "<返回"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 12
+        Me.ListBox1.Location = New System.Drawing.Point(74, 90)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(651, 364)
+        Me.ListBox1.TabIndex = 7
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(74, 472)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(650, 21)
+        Me.ProgressBar1.TabIndex = 8
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(74, 516)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(115, 27)
+        Me.Button5.TabIndex = 9
+        Me.Button5.Text = "添加文件"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(274, 516)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(105, 27)
+        Me.Button6.TabIndex = 10
+        Me.Button6.Text = "清空列表"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button7
+        '
+        Me.Button7.Location = New System.Drawing.Point(488, 516)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(101, 27)
+        Me.Button7.TabIndex = 11
+        Me.Button7.Text = "启动"
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'OpenFileDialog2
+        '
+        Me.OpenFileDialog2.FileName = "OpenFileDialog2"
+        Me.OpenFileDialog2.Multiselect = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.PictureBox1)
+        Me.GroupBox1.Location = New System.Drawing.Point(44, 76)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(592, 431)
+        Me.GroupBox1.TabIndex = 12
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "图片"
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        '
+        'Button8
+        '
+        Me.Button8.Location = New System.Drawing.Point(642, 530)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(118, 31)
+        Me.Button8.TabIndex = 13
+        Me.Button8.Text = "关于"
+        Me.Button8.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(790, 573)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents OpenFileDialog2 As OpenFileDialog
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Button8 As Button
+End Class
---
+++ 图片放大/Form1.vb
@@ -0,0 +1,119 @@
+﻿Public Class Form1
+    Dim n As Integer = 0
+    Dim ii As Integer = 0
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Button4.Visible = False
+        ListBox1.Visible = False
+        Button5.Visible = False
+        Button6.Visible = False
+        Button7.Visible = False
+        ProgressBar1.Visible = False
+        Me.Text = "图片放大"
+        Timer1.Enabled = False
+    End Sub
+    Public Sub Enlarge(strArg As String)
+        Dim p As New Process
+        p.StartInfo.FileName = Environment.CurrentDirectory + "\realesrgan-ncnn-vulkan.exe"
+        p.StartInfo.Arguments = strArg
+        p.StartInfo.UseShellExecute = False
+        p.StartInfo.RedirectStandardError = True
+        p.StartInfo.RedirectStandardOutput = True
+        p.StartInfo.CreateNoWindow = True
+        p.Start()
+        p.BeginErrorReadLine()
+        p.WaitForExit()
+        p.Close()
+        p.Dispose()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim command As String
+        command = "-i """ + TextBox1.Text + """ -o """ + Mid(TextBox1.Text, 1, Len(TextBox1.Text) - 4) + "_output" + Mid(TextBox1.Text, Len(TextBox1.Text) - 3, 4) + """ -n realesrgan-x4plus-anime"
+        Enlarge(command)
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        OpenFileDialog1.ShowDialog()
+        TextBox1.Text = OpenFileDialog1.FileName
+        PictureBox1.Image = Image.FromFile(TextBox1.Text)
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        OpenFileDialog2.ShowDialog()
+        For Each f In OpenFileDialog2.FileNames
+            ListBox1.Items.Add(f)
+            n += 1
+        Next
+        ProgressBar1.Maximum = n
+    End Sub
+    Public Sub Threadsub()
+        ii = 0
+        Dim command As String
+        For Each f In ListBox1.Items
+            command = "-i """ + f + """ -o """ + Mid(f, 1, Len(f) - 4) + "_output" + Mid(f, Len(f) - 3, 4) + """ -n realesrgan-x4plus-anime"
+            Enlarge(command)
+            ii += 1
+        Next
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        ListBox1.Items.Clear()
+        n = 0
+        ProgressBar1.Maximum = n
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        Dim thread1 As Task
+        thread1 = System.Threading.Tasks.Task.Run(Sub() Threadsub())
+        Button7.Enabled = False
+        Timer1.Enabled = True
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Button4.Visible = False
+        ListBox1.Visible = False
+        Button5.Visible = False
+        Button6.Visible = False
+        Button7.Visible = False
+        ProgressBar1.Visible = False
+        Label1.Visible = True
+        TextBox1.Visible = True
+        Button3.Visible = True
+        Button1.Visible = True
+        GroupBox1.Visible = True
+        Button2.Visible = True
+        Me.Text = "图片放大"
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Button4.Visible = True
+        ListBox1.Visible = True
+        Button5.Visible = True
+        Button6.Visible = True
+        Button7.Visible = True
+        ProgressBar1.Visible = True
+        Label1.Visible = False
+        TextBox1.Visible = False
+        Button3.Visible = False
+        Button1.Visible = False
+        GroupBox1.Visible = False
+        Button2.Visible = False
+        Me.Text = "图片放大*批处理"
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        ProgressBar1.Value = ii
+        If ii = ProgressBar1.Maximum Then
+            Timer1.Enabled = False
+            MsgBox("处理完成")
+            Button7.Enabled = True
+
+        End If
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        MsgBox("支持的格式：
+jpg/png
+by AGANG")
+    End Sub
+End Class
